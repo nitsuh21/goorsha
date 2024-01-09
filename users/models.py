c807fe7a6858fc1c02a579c27d19e7b8d1ceed5d
nitsuh <nitsuhdemissew21@gmail.com>
2024-01-09 23:12:42 +0300

    users module

--- users/models.py
+++ users/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class User(models.AbstractUser):
+    firstname = models.CharField(max_length=50)
+    middlename = models.CharField(max_length=50)
+    lastname = models.CharField(max_length=50)
+    email = models.EmailField(max_length=50)
