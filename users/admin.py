5aec96814093db576d661999921d210040e2f34d
nitsuh21 <nitsuhdemissew21@gmail.com>
2024-01-14 00:52:53 +0300

    admin introoduced

--- users/admin.py
+++ users/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 
 from .models import User
+from .models import Connect
 
-# Register your models here.
-admin.register(User)
+admin.site.register(User)
+admin.site.register(Connect)
--- users/views.py
+++ users/views.py
@@ -1,22 +1,36 @@
+from typing import Any
 from django.shortcuts import render
 from requests import Response
 from rest_framework.views import APIView
-from .models import User
+from rest_framework import status
+from rest_framework.generics import CreateAPIView
+from rest_framework.permissions import AllowAny
 from rest_framework_simplejwt.views import TokenObtainPairView
 from .serializers import CustomTokenObtainPairSerializer, UserSerializer    
 
 class CustomTokenObtainPairView(TokenObtainPairView):
     serializer_class = CustomTokenObtainPairSerializer
 
-class UserSignupView(APIView):
-    def post(self, request, format=None):
-        serializer = UserSerializer(data=request.data)
-        if serializer.is_valid():
-            user = User.objects.create_user(
-                serializer.data['username'],
-                serializer.data['email'],
-                serializer.data['password']
-            )
-            user.save()
-            return Response(serializer.data)
-        return Response(serializer.errors)
+class UserSignupView(CreateAPIView):
+    serializer_class = UserSerializer
+    permission_classes = [AllowAny]
+
+    def post(self, request, *args, **kwargs):
+        # Additional logic for user signup if needed
+        response = super().post(request, *args, **kwargs)
+        if response.status_code == status.HTTP_201_CREATED:
+            # Additional logic after successful user signup
+            pass
+        return response
+
+    def get(self, request, *args, **kwargs):
+        return Response("Method 'GET' not allowed.")
+
+    def put(self, request, *args, **kwargs):
+        return Response("Method 'PUT' not allowed.")
+
+    def patch(self, request, *args, **kwargs):
+        return Response("Method 'PATCH' not allowed.")
+
+    def delete(self, request, *args, **kwargs):
+        return Response("Method 'DELETE' not allowed.")
