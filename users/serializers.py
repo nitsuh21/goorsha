90a90cbcf337a9bc1933d57f094e2ae831b1a54f
nitsuh21 <nitsuhdemissew21@gmail.com>
2024-01-13 12:53:30 +0300

    goorsha gang killing it

---
+++ users/serializers.py
@@ -0,0 +1,21 @@
+# myapp/serializers.py
+from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
+from .models import User
+
+class CustomTokenObtainPairSerializer(TokenObtainPairSerializer):
+    @classmethod
+    def get_token(cls, user):
+        token = super().get_token(user)
+
+        return token
+    
+    def validate(self, attrs):
+        data = super().validate(attrs)
+
+        data['user'] = {
+            'id': self.user.id,
+            'username': self.user.username,
+            'email': self.user.email,
+        }
+
+        return data
