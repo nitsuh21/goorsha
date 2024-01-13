d1e5033d16968f6b55119f2cd295dc5db5561e02
nitsuh21 <nitsuhdemissew21@gmail.com>
2024-01-13 11:55:48 +0300

    code cleanup

--- goorsha/settings.py
+++ goorsha/settings.py
@@ -109,8 +109,6 @@ AUTH_PASSWORD_VALIDATORS = [
 ]
 
 
-# Internationalization
-# https://docs.djangoproject.com/en/5.0/topics/i18n/
 
 LANGUAGE_CODE = 'en-us'
 
@@ -121,8 +119,6 @@ USE_I18N = True
 USE_TZ = True
 
 
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
 STATICFILES_DIRS = [
