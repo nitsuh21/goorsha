d56a7805a05595e14fb4925f1613f859e8b78909
nitsuh21 <nitsuhdemissew21@gmail.com>
2024-01-17 17:57:53 +0300

    model changes

--- affiliate/models.py
+++ affiliate/models.py
@@ -1,6 +1,18 @@
 from django.db import models
 
 # Create your models here.
+class Product(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.CharField(max_length=200)
+    image = models.ImageField(upload_to='images/')
+    price = models.DecimalField(max_digits=8, decimal_places=2)
+    affiliate_promo = models.ForeignKey('AffilatePromo', on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    
+    def __str__(self) -> str:
+        return self.name
+
 class AffilatePromo(models.Model):
     name = models.CharField(max_length=100)
     description = models.CharField(max_length=200)
--- affiliate/views.py
+++ affiliate/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
 # Create your views here.
+def createproduct(request):
+    if request.method == 'POST':
+        pass
+    else:
+        pass
