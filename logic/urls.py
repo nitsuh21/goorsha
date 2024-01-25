79cf4c172d1c51ee5b1449960556bbe722e8ffc8
nitsuh21 <nitsuhdemissew21@gmail.com>
2024-01-25 21:50:53 +0300

    url added

--- logic/urls.py
+++ logic/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
-from .views import BusinessList, CampaignList
+from .views import BusinessList, CampaignList , BusinessDetail, CampaignDetail
 
 urlpatterns = [
     path('business/', BusinessList.as_view(), name='business_list'),
-    path('campaign/', CampaignList.as_view(), name='campaign_list')
+    path('campaign/', CampaignList.as_view(), name='campaign_list'),
+    path('business/<int:pk>', BusinessDetail.as_view(),name='business_detail'),
+    path('campaign/<int:pk>',CampaignDetail.as_view(),name='campaign_detail')
 ]
