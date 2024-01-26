d8627fcf16219dbc75db437e2c8b542a86b502b1
nitsuh21 <nitsuhdemissew21@gmail.com>
2024-01-26 21:56:10 +0300

    bemote changes

--- logic/admin.py
+++ logic/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 from .models import Business, Campaign
 
-# Register your models here.
+# Register your models here
 admin.site.register(Business)
 admin.site.register(Campaign)
+
