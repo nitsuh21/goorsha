a0655e7448dd134dc0345c8cdc880a023f0ae314
nitsuh21 <nitsuhdemissew21@gmail.com>
2024-02-01 22:09:58 +0300

    serializers added

--- logic/serializers.py
+++ logic/serializers.py
@@ -1,5 +1,5 @@
 from rest_framework import serializers
-from .models import Business, Campaign
+from .models import Business, Campaign ,Product
 
 class BusinessSerializer(serializers.ModelSerializer):
     class Meta:
@@ -9,4 +9,9 @@ class BusinessSerializer(serializers.ModelSerializer):
 class CampaignSerializer(serializers.ModelSerializer):
     class Meta:
         model = Campaign
+        fields = '__all__'
+
+class ProductSerializer(serializers.ModelSerial):
+    class Meta:
+        model = Product
         fields = '__all__'
