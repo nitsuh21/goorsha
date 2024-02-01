dd2f7411a2e342b429c9e381b37bdf60268fd3ba
nitsuh21 <nitsuhdemissew21@gmail.com>
2024-02-01 21:53:45 +0300

    product model add

--- logic/models.py
+++ logic/models.py
@@ -15,12 +15,28 @@ class Business(models.Model):
     def __str__(self):
         return self.name
 
-class Campaign(models.Model):
-    name = models.CharField(max_length=50)
-    description = models.CharField(max_length=50)
-    business = models.ForeignKey(Business, on_delete=models.CASCADE,related_name='business')
-    affiliates = models.ForeignKey(User, on_delete=models.CASCADE, related_name='affiliates')
+
+class Product(models.Model):
+    name = models.CharField(max_length=255)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    quantity = models.IntegerField(default=0)
+    image = models.ImageField(upload_to='product_images/')
+    published_by = models.ForeignKey(Business, on_delete=models.CASCADE,related_name='business')
 
     def __str__(self):
         return self.name
 
+
+class Campaign(models.Model):
+    name = models.CharField(max_length=50)
+    description = models.CharField(max_length=50)
+    product = models.ForeignKey(Product,on_delete=models.CASCADE,related_name='product')
+    business = models.ForeignKey(Business, on_delete=models.CASCADE,related_name='business')
+    affiliates = models.ForeignKey(User, on_delete=models.CASCADE, related_name='affiliates')
+    no_of_likes = models.IntegerField(blank=True)
+    no_of_visitors = models.IntegerField(blank=True)
+    no_of_sales = models.IntegerField(blank=True)
+
+    def __str__(self):
+        return self.name
